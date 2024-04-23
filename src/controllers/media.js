996731e25f95bdf40a70a3880617942e3d1705a1
DiabeticSanti <sabtiagoramirez2904@gmail.com>
2024-04-23 01:46:54 -0500

    Se completa el modulo de creacion de Media y eliminar, Falta editar

--- src/controllers/media.js
+++ src/controllers/media.js
@@ -1,16 +1,25 @@
 import { pool } from "../db.js";
 
 export const renderMedia = async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM media");
+  const [rows] =
+    await pool.query(`Select med.*, dir.nombre as NombreDirector, gen.nombre as NombreGenero,
+    pro.nombre as NombreProductor, tip.nombre as NombreTipo from media med 
+    inner join director dir on dir.id = med.directorPrincipal
+    inner join genero gen on gen.id = med.generoPrincipal
+    inner join productor pro on pro.id = med.productora
+    inner join tipo tip on tip.id = med.tipo`);
   const [directores] = await pool.query("SELECT * FROM director");
-  res.render("media", { media: rows, directores });
+  const [generos] = await pool.query("SELECT * FROM genero");
+  const [productores] = await pool.query("SELECT * FROM productor");
+  const [tipos] = await pool.query("SELECT * FROM tipo");
+  res.render("media", { media: rows, directores, generos, productores, tipos });
 };
 
 export const createMedia = async (req, res) => {
   const newMedia = req.body;
   newMedia.fechaCreacion = new Date();
   await pool.query("INSERT INTO media set ?", [newMedia]);
-  res.redirect("/");
+  res.redirect("/media");
 };
 
 export const editMedia = async (req, res) => {
@@ -25,7 +34,7 @@ export const updateMedia = async (req, res) => {
   const newMedia = req.body;
   newMedia.fechaActualizacion = new Date();
   await pool.query("UPDATE media set ? WHERE id = ?", [newMedia, id]);
-  res.redirect("/");
+  res.redirect("/media");
 };
 
 export const deleteMedia = async (req, res) => {
@@ -34,5 +43,5 @@ export const deleteMedia = async (req, res) => {
   if (result.affectedRows === 1) {
     res.json({ message: "media eliminado" });
   }
-  res.redirect("/");
+  res.redirect("/media");
 };
